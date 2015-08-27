7af73de8e18320fb934e2eeda580e03dfedfcf0b
flexjoy <flexjoy@ya.ru>
2015-08-27 14:42:48 +0300

    add Url class

---
+++ src/main/java/com/springapp/Url.java
@@ -0,0 +1,8 @@
+package com.springapp;
+/**
+ * Url class
+ * Created by SCherepanov on 27.08.2015.
+ */
+public final class Url {
+    public static final String HOME_PAGE = "/";
+}
--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -1,14 +1,15 @@
 package com.springapp.mvc;
 
+import com.springapp.Url;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
+
+	@RequestMapping(value = Url.HOME_PAGE, method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Привет мир!");
 		return "hello";
