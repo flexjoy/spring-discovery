36e96cc9f17e3a27a1c6a07694be8a643ba49585
flexjoy <flexjoy@ya.ru>
2015-08-28 12:49:04 +0300

    Refactoring.
    No functional changes.

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -4,7 +4,6 @@ import com.springapp.Url;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
