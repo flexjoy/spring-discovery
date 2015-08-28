a086e586390673df0b58fc4371d0a45a20831537
flexjoy <flexjoy@ya.ru>
2015-08-28 12:52:13 +0300

    Change Spring xml-configuration to Java configuration
    Replaces mvc-dispatcher-servlet.xml file

---
+++ src/main/java/com/springapp/config/WebAppConfig.java
@@ -0,0 +1,30 @@
+package com.springapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+/**
+ * Spring Java Configuration class. Replaces mvc-dispatcher-servlet.xml file.
+ *
+ * @author Sergey Cherepanov
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.springapp")
+public class WebAppConfig {
+
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+}
