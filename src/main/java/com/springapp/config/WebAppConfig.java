24935349e3136a2f07293614a7a2cf4bab799c68
flexjoy <flexjoy@ya.ru>
2015-08-31 11:22:54 +0300

    Change UrlBasedViewResolver to InternalResourceViewResolver class.

--- src/main/java/com/springapp/config/WebAppConfig.java
+++ src/main/java/com/springapp/config/WebAppConfig.java
@@ -4,10 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * Spring Java Configuration class. Replaces mvc-dispatcher-servlet.xml file.
@@ -20,11 +17,10 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 public class WebAppConfig {
 
     @Bean
-    public UrlBasedViewResolver setupViewResolver() {
-        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setPrefix("/WEB-INF/pages/");
         resolver.setSuffix(".jsp");
-        resolver.setViewClass(JstlView.class);
         return resolver;
     }
 }
