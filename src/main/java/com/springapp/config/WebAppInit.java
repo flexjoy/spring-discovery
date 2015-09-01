27d9bf6fb4a867047b8d49468ef3c2847f8a7bba
flexjoy <flexjoy@ya.ru>
2015-09-01 13:17:24 +0300

    WebAppInit.java: return to AbstractAnnotationConfigDispatcherServletInitializer implementation
    All beans are moved from root context to servlet context.
    The root context is initialized by empty RootConfig.class
    The servlet context is initialized by ServletConfig.class

--- src/main/java/com/springapp/config/WebAppInit.java
+++ src/main/java/com/springapp/config/WebAppInit.java
@@ -1,35 +1,26 @@
 package com.springapp.config;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 /**
  *  Spring application initializing class. Replaces web.xml file.
  *
  * @author Sergey Cherepanov
  */
-public class WebAppInit implements WebApplicationInitializer {
+public class WebAppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
-    public void onStartup(ServletContext container) throws ServletException {
-        // Создаем корневой (root) контекст Spring
-        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { RootConfig.class };
+    }
 
-        // Регистрируем в контексте конфигурационный класс
-        rootContext.register(WebAppConfig.class);
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { ServletConfig.class };
+    }
 
-        // Листенер для управления жизненным циклом корневого контекста Spring
-        container.addListener(new ContextLoaderListener(rootContext));
-
-        // Регистрируем сервлет-диспетчер Spring MVC
-        ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(rootContext));
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
     }
 }
