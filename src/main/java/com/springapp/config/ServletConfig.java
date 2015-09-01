3cad09522906eb30c7eac11ced8a57552ea30f72
flexjoy <flexjoy@ya.ru>
2015-09-01 13:08:21 +0300

    ServletConfig.class: change vewResolver return value to ViewResolver interface

--- src/main/java/com/springapp/config/ServletConfig.java
+++ src/main/java/com/springapp/config/ServletConfig.java
@@ -3,6 +3,7 @@ package com.springapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -19,7 +20,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 public class ServletConfig {
 
     @Bean
-    public InternalResourceViewResolver viewResolver() {
+    public ViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setPrefix("/WEB-INF/pages/");
         resolver.setSuffix(".jsp");
