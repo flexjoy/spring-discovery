9f4a5e3a139540e7a24e1e6b0cdc27cd4c4551fb
flexjoy <flexjoy@ya.ru>
2015-09-01 13:00:54 +0300

    Add new empty root context configuration class RootConfig.class
    No functional changes.

---
+++ src/main/java/com/springapp/config/RootConfig.java
@@ -0,0 +1,15 @@
+package com.springapp.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Root context configuration class
+ *
+ * @author Sergey Cherepanov
+ */
+@Configuration
+public class RootConfig {
+
+    // Empty class
+
+}
