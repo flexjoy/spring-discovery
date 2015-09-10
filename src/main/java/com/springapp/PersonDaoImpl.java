67989070d8b908480a1e1b0460dd264bec3c2dfc
flexjoy <flexjoy@ya.ru>
2015-09-10 13:33:50 +0300

    PersonDaoImpl.java: remove typecast in method 'findById'
    Use queryForObject() with RowMapper interface

--- src/main/java/com/springapp/PersonDaoImpl.java
+++ src/main/java/com/springapp/PersonDaoImpl.java
@@ -3,6 +3,7 @@ package com.springapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -10,6 +11,9 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -48,10 +52,20 @@ public class PersonDaoImpl implements PersonDao {
     @Override
     public Person findById(long id) {
         String query = "SELECT id, name, age FROM people WHERE id = :id";
-        Map<String, Long> map = new HashMap<>();
-        map.put("id", id);
-        BeanPropertyRowMapper mapper = new BeanPropertyRowMapper(Person.class);
-        return (Person)jdbcTemplate.queryForObject(query, map, mapper);
+        Map<String, Object> map = Collections.singletonMap("id", id);
+        return jdbcTemplate.queryForObject(query, map, new personRowMapper());
+    }
+
+    private class personRowMapper implements RowMapper<Person>
+    {
+        @Override
+        public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+            Person person = new Person();
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setAge(resultSet.getInt("age"));
+            return person;
+        }
     }
 
     private static SqlParameterSource buildParameterSource(Person person) {
