0e3ae11409bc9b9b942fad300d5f1f903c995017
flexjoy <flexjoy@ya.ru>
2015-09-24 14:05:04 +0300

    add using Url.java class constants in all views
    Fix #26

--- src/main/java/com/springapp/Url.java
+++ src/main/java/com/springapp/Url.java
@@ -1,19 +1,22 @@
 package com.springapp;
 
+import org.springframework.stereotype.Component;
+
 /**
  * List of static strings that determine the URL path.
  *
  * @author Sergey Cherepanov
  */
+@Component("Url")
 public final class Url {
-    public static final String HOME_PAGE = "/";
-    public static final String SHOW_PERSON = "/person/show";
-    public static final String ADD_PERSON = "/person/add";
-    public static final String ERROR_PAGE = "/error";
-    public static final String PERSON = "/person/{id}";
-    public static final String DELETE_PERSON = "/person/delete";
-    public static final String EDIT_PERSON = "/person/edit/{id}";
-    public static final String CONFIRM_DELETE = "/confirmDelete/{id}";
-    public static final String LOGIN = "/login";
-    public static final String LOGOUT = "/logout";
+    public static final String HOME_PAGE        = "/";
+    public static final String SHOW_PERSON      = "/person/show";
+    public static final String ADD_PERSON       = "/person/add";
+    public static final String ERROR_PAGE       = "/error";
+    public static final String PERSON           = "/person/{id}";
+    public static final String DELETE_PERSON    = "/person/delete";
+    public static final String EDIT_PERSON      = "/person/edit/{id}";
+    public static final String CONFIRM_DELETE   = "/confirmDelete/{id}";
+    public static final String LOGIN            = "/login";
+    public static final String LOGOUT           = "/logout";
 }
